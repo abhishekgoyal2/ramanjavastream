be0ea687d36b5ca6738e5d83d1f5a1fcfa7d3ff9
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-24 11:02:58 +0530

    Write and Read file

---
+++ src/RamanReadfile.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class RamanReadfile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+//Connecting stream
+		File file=new File("D:\\udemy prac\\filewriting\\textfilereadingraman.txt");
+
+
+		FileReader fr =new FileReader(file);
+		BufferedReader reader=new BufferedReader(fr);
+		
+
+String line ="" ;
+
+		while((line= reader.readLine())!=null ) 
+		{
+		System.out.println(line);
+		
+		}
+//		bufferedread.read();
+		
+
+		reader.close();
+		
+	}
+
+}
--- src/Writingfile.java
+++ src/Writingfile.java
@@ -11,20 +11,23 @@ public class Writingfile {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 //
-		File file=new File("D:/udemy prac/filewriting/textfilewritingraman.csv");
+		File file=new File("D:/udemy prac/filewriting/textfilewritingraman.html");
 		
-		FileWriter fw =new FileWriter(file,false);
+		FileWriter fw =new FileWriter(file,true);
 		BufferedWriter bufferedwriter=new BufferedWriter(fw);
-		for (int i=0;i<4;i++)
-			
-		{
-			for (int j=0;j<3;j++)
-			{
-				int num = (int) (Math.random()*100);
-				bufferedwriter.write(num + "," );
-			}
-			bufferedwriter.newLine();
-		}
+		
+//		for (int i=0;i<4;i++)
+//			
+//		{
+//			for (int j=0;j<3;j++)
+//			{
+//				int num = (int) (Math.random()*100);
+//				bufferedwriter.write(num + "," );
+//			}
+//			bufferedwriter.newLine();
+//		}
+		
+		bufferedwriter.write("<html><body><title>Way2automation</title><h1>Learning Selenium and java from way2autoamtion.com</h1></boyd></html>");
 		
 //		bufferedwriter.newLine();
 //		bufferedwriter.write("First line");
